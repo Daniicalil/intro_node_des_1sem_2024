2ce9f8c11d40a1652f85468b211933a32a075346
Daniicalil <159494730+Daniicalil@users.noreply.github.com>
2024-02-22 20:37:02 -0300

    aula 22-02-24

---
+++ repeticao.js
@@ -0,0 +1,33 @@
+let entrada = require('prompt-sync')();  
+
+// let idade = parseInt(entrada('Digite sua idade: '));
+
+// let idadeInt = parseInt(idade);
+
+// if (idade < 18) {
+//     console.log('Menor');
+// }else if (idade === 18) {
+//     console.log('Tem 18');
+// }else {
+//     console.log('Maior');
+// }
+
+// console.log(`Sua idade é ${idade}`);
+
+let num = parseInt(entrada('Digite um número para visualizar a tabuada!'));
+
+// let cont = 1;
+
+// while (cont <= 10) {
+//     let calc = num * cont;
+//     console.log(`${num} X ${cont} = ${calc}`);
+//     cont++;
+// }
+
+for (let cont = 1; cont <= 10; cont++) {
+    let calc = num * cont;
+    console.log(`${num} X ${cont} = ${calc}`);
+}
+
+//prompt-sync: entrada de dados
+//log: saída de dados
